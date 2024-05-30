3f9547238ded3f25ec66c8f8b35c318398cb6a15
Mario Hernandez <52926248+Marioalf2002@users.noreply.github.com>
2024-05-29 20:03:30 -0500

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import Loader from "@/components/admin-panel/Loader";
-import Login from "@/components/admin-panel/Login";
+// import Login from "@/components/admin-panel/Login";
 import Sidebar from "@/components/admin-panel/Sidebar";
 import { useAppSelector } from "@/redux/hooks";
 import { useSession } from "next-auth/react";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const isLoading = useAppSelector((store) => store.loginReducer);
-  const { data: session } = useSession();
+  // const isLoading = useAppSelector((store) => store.loginReducer);
+  // const { data: session } = useSession();
 
-  if (!session?.user) {
-    return <Login />;
-  }
+  // if (!session?.user) {
+  //   return <Login />;
+  // }
 
   return (
     <div className="flex gap-2">
@@ -21,7 +21,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="w-full h-full">
         <div className="bg-gray-200 p-4 h-screen">{children}</div>
       </div>
-      {isLoading && <Loader />}
+      {/* {isLoading && <Loader />} */}
     </div>
   );
 };
